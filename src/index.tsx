b0970c71e9fc722044571752b137b4b202460eb8
ju-voro <voronetskajaj@hotmail.com>
2023-01-10 21:41:56 +0100

    Sandbox step one

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,27 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>Hello</div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let users = [
+  {
+    name: 'John Doe',
+    age: 30,
+    address: {
+      city: 'London',
+      country: 'UK'
+    }
+    
+  },
+  {
+    name: 'Sarah Parker',
+    age: 25,
+    address: {
+      city: 'Oslo',
+      country: 'Norway'
+    }
+  }
+]
+
+console.log(users[0].address);
